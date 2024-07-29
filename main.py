146024fe625686a6ceaf363a1ce2bf195e87cebb
tsn <tsn@tsn.com>
2024-07-29 05:29:49 -0500

    added more features

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 #Import modules
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout #AWLPV
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout #AWLPVH
 
 #Main app,create objects and settings
-app = QApplication([])  #Allows us to create and execute our app
+app = QApplication([])  #Allows us to create and execute our app; takes in an empty list ALWAYS
 main_window = QWidget() #Object to create a new form (window) that we will be editing
 main_window.setWindowTitle("Random Word Maker") #Now we use the modules methods to alter
 main_window.resize(300, 200) #Adjusting the size of the form
 
 #Create all app objects
+title = QLabel("Random Keywords")
 
-#--->    ALL DESIGN HERE  <---
+text1 = QLabel("?")
+text2 = QLabel("?")
+text3 = QLabel("?")
+
+button1 = QPushButton("Click me")
+button2 = QPushButton("Click me")
+button3 = QPushButton("Click me")
+
+#ALL DESIGN HERE; Getting all the created objects onto our screen
+master_layout = QVBoxLayout() #Creates a column 
+
+row1 = QHBoxLayout() #Creates a row
+row2 = QHBoxLayout() #Creates a row
+row3 = QHBoxLayout() #Creates a row
+
+#Add the objects to the layout using addWidget method; here we can edit the appearance
+row1.addWidget(title, alignment=Qt.AlignCenter) #add a wifget named title with alignment at the center 
+
+row2.addWidget(text1, alignment=Qt.AlignCenter)
+row2.addWidget(text2, alignment=Qt.AlignCenter)
+row2.addWidget(text3, alignment=Qt.AlignCenter)
+
+row3.addWidget(button1) #buttons do not need alignment
+row3.addWidget(button2)
+row3.addWidget(button3)
+
+#Drop them to the master layout; e.g rows and columns are layouts 
+master_layout.addLayout(row1)
+master_layout.addLayout(row2)
+master_layout.addLayout(row3)
+
+main_window.setLayout(master_layout) #we then set the master layout as the main layout
 
 #Events
 
